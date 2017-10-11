ba003ae0d8f3b7e2c727268c304418ef26fe8949
YASUDA Jiro <portown09@gmail.com>
2017-10-11 23:28:51 +0900

    Make password constraints looser.

--- Berima/Models/ManageViewModels/EnableAuthenticatorViewModel.cs
+++ Berima/Models/ManageViewModels/EnableAuthenticatorViewModel.cs
@@ -10,7 +10,7 @@ namespace Berima.Models.ManageViewModels
     public class EnableAuthenticatorViewModel
     {
             [Required]
-            [StringLength(7, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(7, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
             [DataType(DataType.Text)]
             [Display(Name = "Verification Code")]
             public string Code { get; set; }
